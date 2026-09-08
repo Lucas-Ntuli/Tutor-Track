3be565eebd01427f60c1e4025cb7e36b2dc5d50f
Lucas Ntuli <lucasntuli292@gmail.com>
2026-09-08 07:59:36 +0000

    run terraform fmt to fix formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,18 @@
 terraform {
-  
+
   required_version = ">= 1.5.0"
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~> 3.90"
     }
   }
 
   backend "azurerm" {
-  resource_group_name = "tutortrack-tfstate-rg"
-  storage_account_name = "tutortracktfstate"
-  container_name = "tfstate"
-  key = "tutortrack.tfstate"
+    resource_group_name  = "tutortrack-tfstate-rg"
+    storage_account_name = "tutortracktfstate"
+    container_name       = "tfstate"
+    key                  = "tutortrack.tfstate"
   }
 }
 
